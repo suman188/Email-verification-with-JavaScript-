aa3c3b91c239d2d20c3cfe72e0a49a16c6ac555b
vinay suman <sumanvinay18@gmail.com>
2025-01-16 14:53:30 +0530

    add first commit

---
+++ js1.js
@@ -0,0 +1,9 @@
+// Conditions
+// Input Email
+// Length of the email > 11
+// After "." only 2 or 3 characters are allowed
+// Minimum 3 characters between "@" and "."
+
+const email = prompt("Enter an email address");
+const emailLength = email.length;
+console.log(email, emailLength);
