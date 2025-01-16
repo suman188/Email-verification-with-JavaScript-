0486533db7e2757d8646e10c290c54c823dc2576
vinay suman <sumanvinay18@gmail.com>
2025-01-16 19:05:22 +0530

    add final commit

--- js1.js
+++ js1.js
@@ -3,7 +3,17 @@
 // Length of the email > 11
 // After "." only 2 or 3 characters are allowed
 // Minimum 3 characters between "@" and "."
+// exampels: "vinaysuman.123@gmail.com":->
 
 const email = prompt("Enter an email address");
 const emailLength = email.length;
-console.log(email, emailLength);
+const dotIndex = email.lastIndexOf(".");
+const atIndex = email.lastIndexOf("@");
+const lastIndexOf = emailLength - 1
+
+// checking for invalid email...
+if (emailLength < 11 || lastIndexOf - dotIndex < 2 || lastIndexOf - dotIndex >3 || lastIndexOf - atIndex < 3){
+    console.log("Invalid email");
+}else{
+    console.log("Valid email");
+}
